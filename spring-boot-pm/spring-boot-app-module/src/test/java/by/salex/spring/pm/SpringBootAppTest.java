7f0183ce401e80a48e41e8b4afd6a5e15eaeae35
Alexander <alex.shevtsov80@gmail.com>
2024-12-02 17:39:58 +0300

    surefire plugin added

--- spring-boot-pm/spring-boot-app-module/src/test/java/by/salex/spring/pm/SpringBootAppTest.java
+++ spring-boot-pm/spring-boot-app-module/src/test/java/by/salex/spring/pm/SpringBootAppTest.java
@@ -1,7 +1,9 @@
 package by.salex.spring.pm;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 public class SpringBootAppTest {
     @Test
     void contextTest() {
