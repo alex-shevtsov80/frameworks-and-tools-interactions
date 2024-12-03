e6a1f5ca1e190320eec95edf353d28a4a9dd7fee
Alexander <alex.shevtsov80@gmail.com>
2024-12-03 11:17:55 +0300

    JdbcTemplate with sql and custom repository example

--- spring-boot-data/spring-data-jdbc/src/main/java/by/salex/spring/data/repository/ClientRepository.java
+++ spring-boot-data/spring-data-jdbc/src/main/java/by/salex/spring/data/repository/ClientRepository.java
@@ -3,8 +3,8 @@ package by.salex.spring.data.repository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import by.salex.spring.data.entity.Client;
+import by.salex.spring.data.model.Client;
 
 @Repository
-public interface ClientRepository extends CrudRepository<Client, Long>{
+public interface ClientRepository extends CrudRepository<Client, Long>, ClientRepositoryCustom {
 }
