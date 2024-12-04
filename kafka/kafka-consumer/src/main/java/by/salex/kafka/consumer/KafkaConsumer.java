0257984ccc1b65ea0fa07bbea7b9cd8b38417128
Alexander <alex.shevtsov80@gmail.com>
2024-12-04 14:08:08 +0300

    kafka-pc-common module added

---
+++ kafka/kafka-consumer/src/main/java/by/salex/kafka/consumer/KafkaConsumer.java
@@ -0,0 +1,12 @@
+package by.salex.kafka.producer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class KafkaConsumer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaConsumer.class);
+
+    public static void main(String[] args) {
+        LOGGER.info("Starting produces...");
+    }
+}
